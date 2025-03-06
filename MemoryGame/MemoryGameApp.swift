d18bc14cfeda1d5490afa4ca35ed6fa824374385
jslpdcharan <59231349+jslpdcharan@users.noreply.github.com>
2025-03-06 14:07:36 -0600

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Satyanarayana on 06/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
